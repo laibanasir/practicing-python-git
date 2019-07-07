d9b288b4811f101b24848f55b398e102e46b7f56
Laiba Nasir <laibanasirr@gmail.com>
2019-07-07 14:26:23 +0500

    oop

---
+++ area and perimeter of a circle oop.py
@@ -0,0 +1,18 @@
+pi = 3.14
+class circle:
+    def __init__(self, rad):
+        self.rad = rad
+    def area(self):
+        area = pi * self.rad ** 2
+        print('Area of the circle :', area)
+    def perimeter(self, radius):
+        perimeter = 2 * pi * self.rad
+        print('Perimeter of the circle :', perimeter)
+
+cirlce_first = circle(3)
+cirlce_first.area()
+cirlce_first.perimeter()
+
+circle_second = circle(7)
+circle_second.area()
+circle_second.perimeter()
